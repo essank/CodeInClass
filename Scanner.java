98f1c1e0da1733e8c3f277df5014dc245c4d7c9a
Essa ✌️ <essank99@gmail.com>
2016-11-09 13:19:48 -0600

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Example
+{
+   public static void main (String[] args)
+   {
+      Scanner scan = new Scanner(System.in);
+      
+      String name;
+      
+      System.println("what is your name: ");
+      
+      name = in.nextLine;
+      
+      System.out.println("Hello" + name);
+   }
+}
