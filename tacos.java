4700c0839d9a82a00788603f5c44fe24268582b9
Essa ✌️ <essank99@gmail.com>
2016-11-09 14:06:07 -0600

    Create tacos.java

---
+++ tacos.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Example
+{
+   public static void main (String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+      
+      String name;
+      
+      int tacos;
+      
+      double total = 0;
+      
+      double tax;
+      
+      double subTotal;
+      
+      System.out.print("What's your name? ");
+      name = in.nextLine();
+      
+      System.out.print("How many tacos? ");
+      tacos = in.nextInt();
+      
+      System.out.print("What is the tax? ");
+      tax = in.nextInt();
+      
+      //System.out.print("What is the subtotal? ");
+      //subTotal = in.nextInt();
+      
+      subTotal = tacos * .99;
+      total = (tacos * .99) + subTotal;
+      
+      
+      
+      System.out.print("\nThanks " + name + " your subtotal is $" + subTotal + " for " + tacos +
+                         " tacos." + " your tax is " + tax + " your total is " + total);
+   }
+}
